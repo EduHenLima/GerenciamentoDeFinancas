ad36e6d8d314ebee107cd44b39bbaaa416d7ad88
Eduardo Lima <eduardoh.lima17@hotmail.com>
2021-08-09 22:44:18 -0300

    Refactory eloquent and controllers

--- src/Plugins/DbPlugin.php
+++ src/Plugins/DbPlugin.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace SONFin\Plugins;
 
 
+use Interop\Container\ContainerInterface;
+use SONFin\Models\CategoryCosts;
+use SONFin\Repository\RepositoryFactory;
 use SONFin\ServiceContainerInterface;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
@@ -20,5 +23,10 @@ class DbPlugin implements PluginInterface
 
         $capsule->addConnection($config['development']);
         $capsule->bootEloquent();
+
+        $container->add('repository.factory', new RepositoryFactory());
+        $container->addLazy('category-cost.repository', function (ContainerInterface $container){
+            return $container->get('repository.factory')->factory(CategoryCosts::class);
+        });
     }
 }
